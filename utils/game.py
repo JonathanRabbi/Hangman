8d3aea905ca128e37328ad888d80cf0e637d9da8
JonathanRabbi <135423708+JonathanRabbi@users.noreply.github.com>
2023-06-14 08:30:21 +0000

    attributes

--- utils/game.py
+++ utils/game.py
@@ -1,1 +1,8 @@
+class Hangman:
+    def__init__(self, word)
+    self.word_to_find = ['becode']
+    self.lives = 5
+    self.word = word
+    self.correctly_guessed_letters = ["_"] * len(self.word)
+    
 
