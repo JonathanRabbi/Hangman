7ee94885c53961bb82840127dc2317afe7273d54
JonathanRabbi <135423708+JonathanRabbi@users.noreply.github.com>
2023-06-14 08:59:51 +0000

    full list of attributes

--- utils/game.py
+++ utils/game.py
@@ -1,8 +1,13 @@
 class Hangman:
-    def__init__(self, word)
-    self.word_to_find = ['becode']
+    def__init__(self)
+    self.word_to_find = ["becode"]
     self.lives = 5
-    self.word = word
-    self.correctly_guessed_letters = ["_"] * len(self.word)
-    
+    self.correctly_guessed_letters = ["_"] * len(self.word_to_find) #list the letters that the player had correctly guessed
+    self.wrongly_guessed_letters = [] # this will list the letters that the player had wrong
+    self.guesses=[] # this will list the letters that the player has attempted
+    self.turn_count = 0 # this will add the amount of turns the player took
+    self.error_count = 0 # this will add the amount of times the player was wrong
+
+
+
 
